bad73ea39037f0a818dc7e616248c0248d24970e
Keshav Raj <keshav@hubbler.mobi>
2017-10-27 15:17:24 +0530

    Initial Commit

---
+++ HideableUIView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HideableUIView
+//
+//  Created by Keshav Raj on 27/10/17.
+//  Copyright © 2017 Keshav Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
